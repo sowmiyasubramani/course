e687dce7b836c77153778cd3e17a971e28c34478
sowmiyasubramani <you@example.com>
2021-01-29 13:27:31 +0530

    course

---
+++ blog/resources/views/header.blade.php
@@ -0,0 +1,12 @@
+<header class="header">
+	<h1>header view</h1>
+</header>
+<style >
+	.header{
+		background-color:skyblue;
+		color:white;
+		height:100px;
+		width:100%;
+		text-align:center;
+	}
+</style>
